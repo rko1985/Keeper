885272582edd94893094869a1e4890d05600e435
Russell Oyama <russelloyama@gmail.com>
2023-05-12 07:13:14 -1000

    mapping note components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,20 @@ import Heading from './Heading';
 import Note from './Note';
 import Footer from './Footer';
 
-function App(){
+import notes from '../notes'
+
+
+function App() {
     return(
         <div>
             <Heading />
-            <Note />
+            {notes.map( (noteItem) => 
+                <Note 
+                    key={noteItem.key}
+                    title={noteItem.title}
+                    content={noteItem.content}
+                />)
+            }
             <Footer />
         </div>
     )
