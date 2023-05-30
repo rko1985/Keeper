3931b0baccaf5f459387ecb358cbea0dc3ed9efa
Russell Oyama <russelloyama@gmail.com>
2023-05-30 08:17:55 -1000

    create and delete notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 
 import Heading from './Heading';
 import Note from './Note';
 import Footer from './Footer';
-
-import notes from '../notes'
-
+import CreateArea from "./CreateArea";
 
 function App() {
+
+    const [notes, setNotes] = useState([]);
+
+    function addNote(newNote){
+        setNotes(prevNotes => {
+            return[
+                ...prevNotes,
+                newNote
+            ]
+        });
+    }
+
+    function deleteNote(id){
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            })
+        })
+    }
+
     return(
         <div>
-            <Heading />
-            {notes.map( (noteItem) => 
+            <Heading/>
+            <CreateArea onAdd={addNote}/>
+            {notes.map( (noteItem, index) => 
                 <Note 
-                    key={noteItem.key}
+                    key={index}
+                    id={index}
                     title={noteItem.title}
                     content={noteItem.content}
+                    onDelete={deleteNote}
                 />)
             }
             <Footer />
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 function Note(props){
+
+    function handleClick(){
+        const id = props.id;
+        props.onDelete(id)
+    }
+
     return(
         <div className="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
+            <button onClick={handleClick}>DELETE</button>
         </div>
     )
 }
